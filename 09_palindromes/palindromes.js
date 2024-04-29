831087db2052e776833f7a60684eedb686588939
Homero Meireles <homeromeirelesjunior@gmail.com>
2024-04-29 20:36:18 -0300

    chore: solving exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    let textPilha = [];
+    let textInverso = '';
+    
+    let textStripped = text.replace(/[^A-Za-z0-9]/g, '');
 
+    for (let i = 0; i < textStripped.length; i++) {
+        textPilha.push(textStripped.charAt(i));
+    }
+
+    for (let i = textStripped.length; i > 0; i--) {
+        textInverso += textPilha.pop();
+    }
+
+    return textStripped.toLowerCase() == textInverso.toLocaleLowerCase() ? true : false;
 };
 
 // Do not edit below this line
