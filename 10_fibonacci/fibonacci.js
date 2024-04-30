6b330ab06dc9d94c51646b1dfe33e29c1c800fee
Homero Meireles <homeromeirelesjunior@gmail.com>
2024-04-29 21:13:21 -0300

    chore: solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0)  return "OOPS";
+    n = Number(n);
+    if (n <= 1) return n;
+  
+    let fibAnterior = 1;
+    let fibAtual = 1;
+    
+    for (let i = 2; i < n; i++) {
+      let temp = fibAtual;
+      fibAtual = fibAnterior + fibAtual;
+      fibAnterior = temp;
+    }
+    
+    return fibAtual;
 };
 
 // Do not edit below this line
