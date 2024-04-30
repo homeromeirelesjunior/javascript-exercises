3a2fcc981b77985a098a7ce987f8d7881ef5078f
Homero Meireles <homero.meireles@novahaus.com.br>
2024-04-30 09:57:36 -0300

    chore: solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const oldestPerson = people.reduce((oldestPerson, currentPerson) => {
+    const date = new Date();
+   
+    const currentPersonAge = currentPerson.yearOfDeath ? currentPerson.yearOfDeath - currentPerson.yearOfBirth : date.getFullYear() -currentPerson.yearOfBirth;
+    const oldestPersonAge = oldestPerson.yearOfDeath ? oldestPerson.yearOfDeath - oldestPerson.yearOfBirth : date.getFullYear() -oldestPerson.yearOfBirth;
 
+    return currentPersonAge > oldestPersonAge ? currentPerson : oldestPerson; 
+  }, people[0])
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
